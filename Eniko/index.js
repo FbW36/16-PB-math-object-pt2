cc1b6f9272ebd42f310ce8b1d87a2d4a3bdb303a
Enikő Veszelka <encsooo@gmail.com>
2020-10-19 15:48:24 +0200

    bug fixes

--- Eniko/index.js
+++ Eniko/index.js
@@ -6,12 +6,16 @@
 
 // You can test the output ==> https://www.daftlogic.com/projects-hex-colour-tester.htm?hex=FF0066
 
-let variables = "0123456789abcdef";
-let variable1 = variables[Math.floor(Math.random() * 16)];
-let variable2 = variables[Math.floor(Math.random() * 16)];
-let variable3 = variables[Math.floor(Math.random() * 16)];
-let variable4 = variables[Math.floor(Math.random() * 16)];
-let variable5 = variables[Math.floor(Math.random() * 16)];
-let variable6 = variables[Math.floor(Math.random() * 16)];
+// let variables = "0123456789abcdef";
+// let variable1 = variables[Math.floor(Math.random() * 16)];
+// let variable2 = variables[Math.floor(Math.random() * 16)];
+// let variable3 = variables[Math.floor(Math.random() * 16)];
+// let variable4 = variables[Math.floor(Math.random() * 16)];
+// let variable5 = variables[Math.floor(Math.random() * 16)];
+// let variable6 = variables[Math.floor(Math.random() * 16)];
 
-console.log("#" + variable1 + variable2 + variable3 + variable4 + variable5 + variable6);
+// console.log("#" + variable1 + variable2 + variable3 + variable4 + variable5 + variable6);
+
+//realased that I did it all wrong
+
+console.log("#" + Math.floor(Math.random() * 16777215).toString(16));
