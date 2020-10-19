26d7bf061af4c51b638b05f2e98e85b4b9057769
AdrianValdes <adrianvaldeshdz@.gmail.com>
2020-10-19 21:34:04 +0200

    Fixed the range

--- adrian/script.js
+++ adrian/script.js
@@ -1,3 +1,3 @@
-let baseForHex = Math.floor(Math.random() * (16777215 - 1));
+let baseForHex = Math.floor(Math.random() * 16777215);
 let hexadecimal = baseForHex.toString(16);
 console.log(hexadecimal);
