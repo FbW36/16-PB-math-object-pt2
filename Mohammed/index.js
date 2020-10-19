90e62e422bad4230c17d1132d625d5040bf14a14
mashraee <m.almashraee@gmail.com>
2020-10-19 16:00:01 +0200

    Exercise 16 -mohammed

---
+++ Mohammed/index.js
@@ -0,0 +1,8 @@
+
+let hexColor = Math.floor(Math.random() * 16777215);
+// 16: converts into Hexadecimal number system
+hexColor = hexColor.toString(16);
+console.log(`#${hexColor}`);
+
+//check the generated hexColors using this website
+// https://www.daftlogic.com/projects-hex-colour-tester.htm?hex=FF0066
