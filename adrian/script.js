c35f7298bdc7e09dbb01080c5cb718f1ee5ce6a8
AdrianValdes <adrianvaldeshdz@.gmail.com>
2020-10-20 16:49:18 +0200

    Adding the last value

--- adrian/script.js
+++ adrian/script.js
@@ -1,3 +1,3 @@
-let baseForHex = Math.floor(Math.random() * 16777215);
+let baseForHex = Math.floor(Math.random() * (16777215 - 0 + 1) + 0);
 let hexadecimal = baseForHex.toString(16);
 console.log(hexadecimal);
